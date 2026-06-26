3c2053790324bac9be1fe4a3b7c11a6385a931b4
hyeonseo1109 <alsgustj2@naver.com>
2026-06-26 19:21:56 +0900

    chore: 환경 설정

---
+++ babel.config.cjs
@@ -0,0 +1,22 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@app": "./src/app",
+            "@pages": "./src/pages",
+            "@widgets": "./src/widgets",
+            "@features": "./src/features",
+            "@entities": "./src/entities",
+            "@shared": "./src/shared",
+          },
+        },
+      ],
+    ],
+  };
+};
