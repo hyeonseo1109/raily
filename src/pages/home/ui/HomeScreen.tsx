638692c2859042c585d6cc504e13ddbdd475efd0
hyeonseo1109 <alsgustj2@naver.com>
2026-06-27 02:18:01 +0900

    feat: css 파일 연결

--- src/pages/home/ui/HomeScreen.tsx
+++ src/pages/home/ui/HomeScreen.tsx
@@ -1,8 +1,9 @@
 import { View, Text } from "react-native";
+import { styles } from "./style";
 
 export const HomeScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home</Text>
     </View>
   );
---
+++ src/pages/home/ui/style.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#c0defa",
+    width: "100%",
+    height: "100%",
+  },
+});
