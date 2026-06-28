d509ef3083661bf9cd9af2e81a61dab48ca29210
hyeonseo1109 <alsgustj2@naver.com>
2026-06-29 03:02:45 +0900

    faet: 로그인팝업

--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,12 +1,19 @@
-import { Image, View, Text } from "react-native";
+import { ImageBackground, View, Text } from "react-native";
 import { loginBackground } from "@shared/assets/images";
 import { styles } from "./style";
 
 export const LoginPage = () => {
   return (
-    <View style={styles.container}>
-      <Image source={loginBackground} style={styles.backgroundImage} />
-      <Text style={styles.text}>로그인페이지</Text>
-    </View>
+    <ImageBackground
+      source={loginBackground}
+      style={styles.container}
+      resizeMode="cover"
+    >
+      <View style={styles.overlay}>
+        <View style={styles.popup}>
+          <Text style={styles.text}>로그인페이지</Text>
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
--- src/pages/login/ui/style.ts
+++ src/pages/login/ui/style.ts
@@ -3,19 +3,38 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#c0defa",
-    width: "100%",
-    height: "100%",
   },
 
-  backgroundImage: {
-    width: "100%",
-    height: "100%",
-    zIndex: 0,
+  overlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.2)",
+  },
+
+  popup: {
+    backgroundColor: "#fffcf9",
+    width: "50%",
+    height: "30%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+
+    boxShadow: [
+      {
+        offsetX: 0,
+        offsetY: 0,
+        blurRadius: 12,
+        spreadDistance: 5,
+        color: "rgba(0,0,0,0.18)",
+        inset: true,
+      },
+    ],
   },
 
   text: {
-    zIndex: 1,
     fontSize: 25,
+    fontWeight: "700",
+    color: "#000000",
   },
 });
